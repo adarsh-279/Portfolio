c07478371848b64c1ef4d4970c18d279d9582927
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-06-10 20:30:19 +0530

    Added ScrollTrigger

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
+
+gsap.registerPlugin(ScrollTrigger);
+
+const mm = gsap.matchMedia();
+
+mm.add("(min-width: 768px)", () => {
+    Shery.mouseFollower({
+      //Parameters are optional.
+      skew: true,
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 1,
+    });
+    
+    Shery.makeMagnet(".nav .name, .nav  a" /* Element to target.*/, {
+      //Parameters are optional.
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 0.5,
+    });
+    
+});
